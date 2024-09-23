7a636dac7dd21afe117708fc0c94f0f0c9e59e5e
Levi Klingler <klingler27@gmail.com>
2024-09-23 01:31:27 -0400

    add `privacy-policy` page

--- src/components/sticky-header.tsx
+++ src/components/sticky-header.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { cn } from '@/lib/utils'
+import { Github } from 'lucide-react'
 
 export function StickyHeader() {
   const [scrollY, setScrollY] = useState(0)
@@ -11,32 +12,50 @@ export function StickyHeader() {
   }, [])
 
   return (
-      <header 
-        className={cn(
-          'sticky top-0 z-50 bg-background shadow-md transition-all',
-          scrollY > 0 ? 'py-2' : 'py-4'
-        )}
-      >
-        <div className="container mx-auto px-4 flex justify-between items-center">
-          <h1 className='text-3xl font-normal font-dancing-script cursor-default'>
-            <span className="text-teal-200">geeked</span>
-            <span className="text-cyan-300 ml-0.5">.</span>
-            <span className="text-cyan-50">gg</span>
-          </h1>
-          <div className="flex items-center space-x-4">
-            <nav>
-              <ul className="flex space-x-4">
-                {['Home', 'Services', 'Contact'].map((item) => (
-                  <li key={item}>
-                    <a href={item === 'Contact' ? 'mailto:levikl@geeked.gg' : `#${item.toLowerCase()}`} className="text-gray-300 hover:text-white">
-                      {item}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
+    <header
+      className={cn(
+        'sticky top-0 z-50 bg-background shadow-md transition-all',
+        scrollY > 0 ? 'py-2' : 'py-4',
+      )}
+    >
+      <div className='container mx-auto px-4 flex justify-between items-center'>
+        <h1 className='text-xl sm:text-3xl font-normal font-dancing-script cursor-default'>
+          <span className='text-teal-200'>geeked</span>
+          <span className='text-cyan-300 ml-0.5'>.</span>
+          <span className='text-cyan-50'>gg</span>
+        </h1>
+        <div className='flex items-center space-x-4'>
+          <nav>
+            <ul className='flex items-center justify-center space-x-4 text-xs sm:text-sm lg:text-base'>
+              <li>
+                <a href='/' className='text-gray-300 hover:text-white'>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='/#services' className='text-gray-300 hover:text-white'>
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href='mailto:levikl@geeked.gg' className='text-gray-300 hover:text-white'>
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://github.com/levikl'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  aria-label='GitHub'
+                >
+                  <Github className='size-4 sm:size-5 lg:size-6 text-gray-300 hover:text-white' />
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </header>
+      </div>
+    </header>
   )
 }
