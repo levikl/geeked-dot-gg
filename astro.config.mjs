600a0abb771615fe2a3f966ef59439ab5448a599
Levi Klingler <levikl@geeked.gg>
2026-01-27 00:40:12 -0500

    bump astro + add robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  site: "https://geeked.gg",
+
+  integrations: [tailwind({ applyBaseStyles: false }), sitemap(), robotsTxt()],
 });
+
